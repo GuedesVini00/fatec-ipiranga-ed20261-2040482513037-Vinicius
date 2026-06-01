3f868170b1488c939b6ce83315b6d3bdcc6e1422
Vinicius Guedes <visoriano007@gmail.com>
2026-05-31 21:50:15 -0300

    Create Exercicio03-A.c

---
+++ ED-20261-Atividades-N2/ED-20261-Atividade-N2-3/Exercicio03-A.c
@@ -0,0 +1,22 @@
+/* RA: 2040482513037 - VINICIUS GUEDES SORIANO PEREIRA */
+#include <stdio.h>
+
+void hanoi(int n, char origem, char destino, char auxiliar)
+{
+    if (n == 1)
+    {
+        printf("Mover disco 1 de %c para %c\n", origem, destino);
+        return;
+    }
+
+    hanoi(n - 1, origem, auxiliar, destino);
+    printf("Mover disco %d de %c para %c\n", n, origem, destino);
+    hanoi(n - 1, auxiliar, destino, origem);
+}
+
+int main(void)
+{
+    hanoi(3, 'A', 'C', 'B');
+
+    return 0;
+}
